0fae9815050ae194bf9173251079799fae82bd8d
unknown <bankerice@gmail.com>
2019-11-07 19:16:01 -0500

    Parsed main page

--- .vscode/extractor.py
+++ .vscode/extractor.py
@@ -1,4 +1,4 @@
-#import re
+import re
 import string
 
 
@@ -6,9 +6,12 @@ from robobrowser import RoboBrowser
 from bs4 import BeautifulSoup
 
 def main():
-
-    #Acesses Genesis
     br = RoboBrowser()
+
+    mainpageGrades = extractMainPage(br)
+
+def extractMainPage(robo):
+    br = robo
     br.open("https://parents.chclc.org/genesis/sis/view?gohome=true")
     form = br.get_form()
     form["j_username"] = "3010476@chclc.org"
@@ -18,36 +21,36 @@ def main():
     #Converts the HTML of the grade page into a string
     src = str(br.parsed())
 
-    #Removes all forms of whitespace
-    src = src.translate({ord(c): None for c in string.whitespace})
-
-    #Removes all Javascript
+    #Removes initial Javascript
     src = src[src.find('<!-- Start of Header-->')+len('<!-- Start of Header-->'): len(src)]
 
+    
     #Removes all HTML tags
-    soup = BeautifulSoup(src)
+    soup = BeautifulSoup(src,"lxml")
     src = ''.join(soup.findAll(text=True))
 
-    
+    #Removes all tabs and newlines
+    src = " ".join(src.split())
 
+    #Cuts the string into the important parts
     notDone = True
-    i = src.find('%')-6
+    i = src.find('Fri')+3 #Consistent and close reference point
     while(notDone):
-        i -= 1
-        if(ord(src[i])>=48 and ord(src[i])<=57):
+        i += 1
+        if(ord(src[i])>=57):
             notDone = False
-    
-    src = src[i+1:src.rfind('%')+1]
-
-    print(src)
-    #print(src[src.find(start)+len(start):src.find(end)+2])
+    src = src[i+2:src.rfind('%')+1]
 
 
+    #Parses the text
+    grades = src.split('%')
+    grades.pop()
+    for i in range(len(grades)):
+        grades[i] = grades[i].split("Email:")
+        if(i != 0):
+            grades[i][0] = grades[i][0][11:len(grades[i][0])]
 
-    #print(re.search('%s(.*)%s' % (start, end), src))
-
-    #result = re.search('%s(.*)%s' % (start, end), src).group(1)
-    #print(result)
+    return grades
 
 
 if __name__ == '__main__':
