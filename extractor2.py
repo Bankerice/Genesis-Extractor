dd99d433f718c8d19533371aac9b938ab0763b49
JKL003 <justinaklam@gmail.com>
2019-11-26 12:14:23 -0500

    Add existing file

--- extractor2.py
+++ extractor2.py
@@ -15,6 +15,7 @@ studentName = ""
 studentID = ""
 courses = list(map(course.Course,[]))
 periods = ['A','B','C','D','E','F','G','H']
+mp = 2
 
 # Body
 def main():
@@ -30,11 +31,12 @@ def extractMainPage(robo):
     form["j_username"] = "@chclc.org"
     form["j_password"] = ""
     br.submit_form(form)
-
+  
     #Converts the HTML of the Summary page into a string and uses it to create courses list of Course objects
     studentID = ""
     br.open("https://parents.chclc.org/genesis/parents?tab1=studentdata&tab2=studentsummary&action=form&studentid="+studentID)
     src = str(br.parsed())
+    #print(src)
     studentID = src[src.find("Student ID")+34:src.find("Student ID")+41]
     
     courseSchedule = src[src.find('Teacher'):len(src)]
@@ -42,71 +44,73 @@ def extractMainPage(robo):
     soup = BeautifulSoup(courseSchedule,"lxml")
     courseSchedule = ''.join(soup.findAll(text=True))
     createCourseList(courseSchedule)
-    # for i in range(len(courses)):
-    #     print(courses[i].period+": " + courses[i].courseName + "\t" + courses[i].teacherName)
+
+
 
     #Converts the HTML of the grade page into a string
     #br.open("document.frmHome.action='parents?tab1=studentdata&tab2=gradebook&tab3=listassignments&studentid="+studentID+"&action=form&date="+)
     br.open("https://parents.chclc.org/genesis/parents?tab1=studentdata&tab2=gradebook&tab3=weeklysummary&action=form&studentid="+studentID)
     src = str(br.parsed)
     
-    
     links = br.get_links()
     urls = [link.get("href") for link in links]
     for i in range(len(links)):
-        #print(links[i])
         if str(links[i]).__contains__("listassignments"):
             br.follow_link(links[i])
             a = str(br.parsed)
             a = " ".join(a.split())
+
+    # CREATE ASSIGNMENT LIST
+    # Determine course codes for each course
+    for i in range(len(courses)):
+        code = src[src.find(courses[i].courseName[0:7])-90:src.find(courses[i].courseName[0:7])+10]
+        if(len(code)>0):
+            code = code[code.find(",")+2:code.find(")")-1]
+            courses[i].code = code[0:code.find(":")]
+            courses[i].section = code[code.find(":")+1:len(code)+1]
+
+    # Create assignment list for each course from course code
+    for i in range(len(courses)):
+        br.open("https://parents.chclc.org/genesis/parents?tab1=studentdata&tab2=gradebook&tab3=coursesummary&studentid=" + str(studentID) + "&action=form&courseCode=" + str(courses[i].code) + "&courseSection=" + str(courses[i].section) + "&mp=2")
+        a = str(br.parsed)
             
-            #print(a)
-            courseN = a[a.find("name=\"fldCourse\""):len(a)]
-            courseN = courseN[courseN.find("option selected"):courseN.find("</select>")]
-            courseN = courseN[courseN.find(">")+1:courseN.find("<")-1]
-            if (len(courseN)>0):
-                courseN = " ".join(courseN.split())
-            
-            assignment = a[a.find("<b>"):a.find("</b>")]
-            assignment = assignment[3:len(assignment)]
-            
-            aCategory = a[a.find("Close Window"):len(a)]
-            aCategory = aCategory[aCategory.find("</div>")+6:aCategory.find("<td")]
-            aCategory = " ".join(aCategory.split())
-            aCategory = aCategory[0:aCategory.find("</td>")-1]
-            
-            aDate = a[a.find("listroweven"):len(a)]
-            aDate = aDate[aDate.find("</div>")+1:aDate.find("style")]
-            aDate = aDate[aDate.find("<div>")+5:aDate.find("</div>")]
-            if (len(aDate)>0):
-                yr = 0
-                if (int(datetime.datetime.today().month) >= int(aDate[0:aDate.find("/")])):
-                    yr = datetime.datetime.today().year
-                else:
-                    yr = datetime.datetime.today().year - datetime.timedelta(365)
-                aDate = datetime.datetime(yr,int(aDate[0:aDate.find("/")]),int(aDate[aDate.find("/")+1:len(aDate)]))
-            
-            aGrade = a[(a.find(" / "))-5:(a.find(" / "))+5]
-            aGrade = " ".join(aGrade.split())
-            aPtsRec = aGrade[aGrade.find(">")+2:aGrade.find(" / ")]
-            aPtsWorth = aGrade[aGrade.find(" / ")+3:len(aGrade)]
-            if (len(aPtsWorth)>0):
-                intPtsWorth = int(aPtsWorth)
-                intPtsRec = int(aPtsRec)
-                for i in range(len(courses)):
-                    if (str(courses[i].courseName) == courseN):
-                        courses[i].addAssignment(assignment,intPtsWorth,intPtsRec,aCategory,aDate.date)
-                        print(courses[i].courseName)
-                        print(courses[i].assignments[0].assignmentName + "\t" + str(courses[i].assignments[0].numPointsReceived))
+        assignment = a[a.find("<b>"):a.find("</b>")]
+        assignment = assignment[3:len(assignment)]
+        print(assignment)
+        
+        aCategory = a[a.find("Close Window"):len(a)]
+        aCategory = aCategory[aCategory.find("</div>")+6:aCategory.find("<td")]
+        aCategory = " ".join(aCategory.split())
+        aCategory = aCategory[0:aCategory.find("</td>")-1]
+        print(aCategory)
+        aDate = a[a.find("listroweven"):len(a)]
+        aDate = aDate[aDate.find("</div>")+1:aDate.find("style")]
+        aDate = aDate[aDate.find("<div>")+5:aDate.find("</div>")]
+        if (len(aDate)>0):
+            yr = 0
+            if (int(datetime.datetime.today().month) >= int(aDate[0:aDate.find("/")])):
+                yr = datetime.datetime.today().year
+            else:
+                yr = datetime.datetime.today().year - datetime.timedelta(365)
+            aDate = datetime.datetime(yr,int(aDate[0:aDate.find("/")]),int(aDate[aDate.find("/")+1:len(aDate)]))
+        print(aDate)
+
+        a = " ".join(a.split())
+        aGrade = a[(a.find(" / "))-5:(a.find(" / "))+5]
+        #aGrade = " ".join(aGrade.split())
+        aPtsRec = aGrade[aGrade.find(">")+2:aGrade.find(" / ")]
+        aPtsWorth = aGrade[aGrade.find(" / ")+3:len(aGrade)]
+        print(aGrade)
+        print(aPtsRec)
+        print(aPtsWorth)
+        if (len(aPtsWorth)>0):
+            print("asdfasdf")
+            intPtsWorth = int(aPtsWorth)
+            intPtsRec = int(aPtsRec)
+            courses[i].addAssignment(assignment,intPtsWorth,intPtsRec,aCategory,aDate.date)
+            print(courses[i].courseName)
             
 
-
-    # Failed attempt to change course to view another course's assignments
-    # forms = br.get_forms()
-    # f = forms[0]
-    # f['fldCourse'].value = "130A:3"
-    # br.submit_form(f)
-    
     
 
 
@@ -143,28 +147,12 @@ def extractMainPage(robo):
         cName = cInfo[0:cInfo[0:cInfo.index(",")].rfind(" ")]
         cTeacher = cInfo[cInfo[0:cInfo.index(",")].rfind(" ")+1:len(cInfo)-1]
 
-        # Create course - unnecessary if already created from Summary page
-        # for i in courses:
-        #     if(str(courses[i].courseName) == cName):
-        #         print("already there: " + cName)
-        #     else:
-        #         c = course.Course(cName,cTeacher,"P",True)
-        # # Add assignments from List Assignments tab (click on link) 
-        # # Example: c.addAssignment("a1",10,10,assignment.Category.MajorAssessments,datetime.datetime.today().date)
-        #         courses.append(c)
-        #         print(cName)
-    
-    # Test by printing info
-    #print("Student: " + studentName)
-    # for i in range(len(courses)):
-    #     print(str(courses[i].currentMPGrade) + " in " + courses[i].courseName 
-    #     + " with " + courses[i].teacherName + " during " + courses[i].period)
 
     
    
 def createCourseList(courseSchedule):
     #Creates courses list of Course objects from info on Summary page
-
+    
     #Ignore LBs
     while(courseSchedule.__contains__("L1\n")):
         lb1 = courseSchedule[courseSchedule.find('L1\n'):len(courseSchedule)]
@@ -183,14 +171,11 @@ def createCourseList(courseSchedule):
         if courseSchedule[i].startswith("\n"):
             courseSchedule[i] = courseSchedule[i][1:len(courseSchedule[i])]
         courseSchedule[i] = courseSchedule[i].split("\n")
-        #print(courseSchedule[i])
-        # for j in range(len(courses)):
-        #     if (str(courses[j].courseName) == str(courseSchedule[i][1]) & courses[j].fullYear)==False:
         courses.append(course.Course(courseSchedule[i][1],courseSchedule[i][5],courseSchedule[i][0],True if courseSchedule[i][2]=='FY' else False))
+
     # for i in range(len(courses)):
-    #     print(courses[i].period + ": " + courses[i].courseName + "\t" + courses[i].teacherName)
-    for i in range(len(courses)):
-        print(courses[i].courseName)
+    #     print(courses[i].courseName)
+
 
 if __name__ == '__main__':
     main()
